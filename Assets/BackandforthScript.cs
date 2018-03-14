8b1c7b6b71fa12193658c4402c4982a53dc08377
Patrick Carroll <patrickgesarcarroll@gmail.com>
2018-03-13 17:29:00 -0700

    Starting on stage 1

---
+++ Assets/BackandforthScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BackandforthScript : MonoBehaviour {
+    // Use this for initialization
+    double timer;
+    void Start () {
+        timer = 0;
+	}
+    
+	// Update is called once per frame
+	void Update () {
+        timer = timer + .01;
+        //Debug.Log(Time.deltaTime);
+        float z = (float)Math.Sin(timer);
+        transform.Translate(0, 0, z);
+	}
+}
